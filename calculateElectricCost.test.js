534baf982e56b2b7e4c98683978824da7b69d7a3
UncleBonn <bonnmyname@gmail.com>
2023-03-06 23:10:16 +0700

    update more invalid argument

--- calculateElectricCost.test.js
+++ calculateElectricCost.test.js
@@ -1,7 +1,8 @@
 const calculateElectricCost = require('./calculateElectricCost');
 
-describe('calculateElectricCost', () => {
+describe('calculate Electric Cost:', () => {
     it('with valid argument', () => {
+        expect(calculateElectricCost(5.8)).toBeCloseTo(9.7324);
         expect(calculateElectricCost(50)).toBeCloseTo(83.9);
         expect(calculateElectricCost(100)).toBeCloseTo(170.6);
         expect(calculateElectricCost(200)).toBeCloseTo(372);
@@ -11,13 +12,32 @@ describe('calculateElectricCost', () => {
         expect(calculateElectricCost(500)).toBeCloseTo(1201.7);
     })
 
-    it('with unvalid argument', () => { 
+    it('with invalid argument', () => { 
         expect(() => calculateElectricCost('a')).toThrow();
         expect(() => calculateElectricCost(null)).toThrow();
         expect(() => calculateElectricCost(undefined)).toThrow();
         expect(() => calculateElectricCost(-10)).toThrow();
         expect(() => calculateElectricCost([1,2,3])).toThrow();
         expect(() => calculateElectricCost({name:'khoi'})).toThrow();
-        // expect(() => calculateElectricCost('')).toThrow();
+        expect(() => calculateElectricCost('')).toThrow();
+        expect(() => calculateElectricCost(false)).toThrow();
+        expect(() => calculateElectricCost(true)).toThrow();
      })
 })
+
+
+describe('calculate Electric Cost Using Parameterized: ', () => {
+    test.each([
+      [5.8, 9.7324],
+      [50, 83.89999999999999],
+      [100, 170.6],
+      [200, 372],
+      [300, 625.6],
+      [400, 909],
+      [500, 1201.7],
+      //n  //expected
+    ])('Amount of kwh: %f - Cost: %f', (kwh, expected) => {
+      expect(calculateElectricCost(kwh)).toEqual(expected);
+            //actual                  ||      //expected
+    });
+});
